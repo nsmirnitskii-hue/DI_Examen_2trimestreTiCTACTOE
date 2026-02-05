3dd806aab5ff9b8c5aaee4edd75a577889b91ef0
Nikolai <n.smirnitskii@ikasle.eus>
2026-02-05 11:55:38 +0100

    he puesto el enunciado predeterminado

--- app/src/main/java/com/example/tictactoe/TicTacToeScreen.kt
+++ app/src/main/java/com/example/tictactoe/TicTacToeScreen.kt
@@ -199,3 +199,53 @@ fun board(state: GameState, vm: TicTacToeViewModel){
         }
     }
 }
+//TO DO:
+//Vamos a trabajar con el estate guardado en el viewModel
+//Hacemos que el contenido de la ventana se ajuste todo lo posible a la ventana
+// y que tenga un padding de 16.dp y este centrado horizontalmente
+
+//Anadimos el titulo 3 en raya usando tipografia definida en los estilos del proyecto
+//Anadimos un espacio de 8.dp
+//Anadimos dos botones para seleccionar la dificultad
+//El boton que este seleccionado tiene que tener el color purple40 definido en Color.kt
+//Al seleccionar el botón usaremos la funcion del viewmodel setDifficulty()
+
+//Anadimos un espacio de 12.dp
+
+//Para visualizar las partidas usamos Card (a poder ser)
+//que ocupe el mayor espacio posible de ancho
+//usa el parametro  elevation = CardDefaults.cardElevation(6.dp)
+//para hacer sombra.
+//para visualizar el contador de las partidas ganadas usamos state.score.human
+//state.score.ai, state.score.draws usa una de las tipografias del tema del proyecto.
+
+//Anade un espacio de 20.dp
+
+//cuando haya ganador (state.winner) anade:
+/*when () {
+    "X" -> "¡Has ganado! 🎉"
+    "O" -> "Gana la máquina 🤖"
+    "Draw" -> "Empate 😐"
+    else -> "Tu turno"*/
+
+//Anadimos un espacio de 12.dp
+
+//Anadimos el tablero:
+//Para ello utilizamos el componente card con estos parametros
+/*elevation = CardDefaults.cardElevation(8.dp),
+    shape = RoundedCornerShape(16.dp)*/
+//Dentro de la card usamos otras estructuras...
+//A dichas estructuras les damos el color de fondo del surface
+//y un padding de 12.dp
+//en cada celda dibujamos el valor del state.board correspondiente
+//A cada celda le damos las siguientes caracteristicas:
+//tamano 90.dp
+//borde 1.dp y Color.Gray
+//Valores centrados y con tamano de 36.dp
+//Cuando se hace click se llama a vm.onCellClicked(index)
+
+//Anadimos un espacio de 20.dp
+//Anadimos boton de nueva ronda llamando al metodo resetBoard()
+//del view model
+
+//Anadimos boton reset total llamando al metodo resetAll() del view model
